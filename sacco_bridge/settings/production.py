2623d86a4b04a6cea20ec7fe27adaf9d0d65146f
gitauwilly1 <gitauwilly254@gmail.com>
2026-06-10 14:47:58 +0300

    change email backend from console to smtp

--- sacco_bridge/settings/production.py
+++ sacco_bridge/settings/production.py
@@ -27,6 +27,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 
 # Sentry
 if os.environ.get('SENTRY_DSN'):
