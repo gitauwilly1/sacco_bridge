d25519ee722c3b09dfcbba71b76cb6565eb96dc8
William Gitau <162879882+gitauwilly1@users.noreply.github.com>
2026-06-18 13:26:23 +0300

    add missing import

--- apps/core/exceptions.py
+++ apps/core/exceptions.py
@@ -1,6 +1,6 @@
 import logging
 import traceback
-
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 from django.utils import timezone
 from rest_framework import status
