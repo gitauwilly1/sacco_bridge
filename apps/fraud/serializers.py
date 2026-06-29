5565773b5090b5d2ff1dbae2850b8fb50dc7a02b
gitauwilly1 <gitauwilly254@gmail.com>
2026-06-29 11:52:03 +0300

    Enhance fraud detection serializers with additional validation

--- apps/fraud/serializers.py
+++ apps/fraud/serializers.py
@@ -1,5 +1,19 @@
 from rest_framework import serializers
-from apps.fraud.models import TransactionRiskAssessment
+from apps.fraud.models import TransactionRiskAssessment, DeviceFingerprint
+
+class DeviceFingerprintSerializer(serializers.ModelSerializer):
+    user_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = DeviceFingerprint
+        fields = [
+            'id', 'user', 'user_name', 'fingerprint',
+            'ip_address', 'user_agent', 'first_seen',
+            'last_seen', 'is_trusted', 'transaction_count'
+        ]
+
+    def get_user_name(self, obj):
+        return obj.user.get_full_name()
 
 
 class RiskAssessmentSerializer(serializers.ModelSerializer):
